7c9477746b9d4daece4ee916978d8c10028e7b10
Marcin Dulak <dulak@fysik.dtu.dk>
2014-05-28 10:03:35 +0200

    rename gaussian

--- accts/Gaussian/09_D01/H2O.sh
+++ accts/Gaussian/09_D01/H2O.sh
@@ -9,4 +9,4 @@ export g09root=/appl/hgauss/Gaussian_09_D01
 . $g09root/g09/bsd/g09.profile
 
 # water example from http://www.gaussian.com/g_tech/g_ur/m_input.htm
-( echo %NProcShared=`cat $PBS_NODEFILE | wc -l`; cat H2O.inp ) | g09 > H2O.out
+( echo %NProcShared=`cat $PBS_NODEFILE | wc -l`; cat H2O.com ) | g09 > H2O.log
