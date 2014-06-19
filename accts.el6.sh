62bae308b1d2b3dc0273a6d1ffa4f8d75c6033ff
marcindulak <Marcin.Dulak@gmail.com>
2014-06-19 15:50:41 +0200

    mpi

--- accts.el6.sh
+++ accts.el6.sh
@@ -1,11 +1,13 @@
+#email=dulak@fysik.dtu.dk,sebo@dtu.dk
 email=dulak@fysik.dtu.dk
+#email=dulak@fysik.dtu.dk,marcin.dulak@gmail.com
 
 # scipy.weave!                                                             
 python_compiled=`readlink -f ~/.python26_compiled`
 
-date=$(date +"%Y-%m-%d")
+#date=`/bin/date +"%Y-%m-%d"`
 
-# Saturday
+# Monday morning
 #15 05 * * 6 rm -rf ${python_compiled} && touch ~/accts/results.out && mkdir -p ~/accts/results && /bin/rm -f ~/accts/results.out.previous && /bin/mv -f ~/accts/results.out ~/accts/results.out.previous && /bin/rm -rf ~/accts/results.previous && /bin/mv -f ~/accts/results ~/accts/results.previous && mkdir -p ~/accts/results && /bin/rm -f ~/.matplotlib/fontList.cache ; export PYTHONPATH=/zhome/c9/d/40082/accts:${PYTHONPATH} && export PYTHONDONTWRITEBYTECODE=1 && python -tt -Wd ~/accts/accts.el6.py --dir ~/accts/results --email ${email} > ~/accts/results.out
 #/bin/rm -rf ${python_compiled} && touch ~/accts/results.out && mkdir -p ~/accts/results && /bin/rm -f ~/accts/results.out-${date} && /bin/mv -f ~/accts/results.out ~/accts/results.out-${date} && /bin/rm -rf ~/accts/results-${date} && /bin/mv -f ~/accts/results ~/accts/results-${date} && mkdir -p ~/accts/results && /bin/rm -f ~/.matplotlib/fontList.cache && export PYTHONDONTWRITEBYTECODE=1 && export PYTHONPATH=`readlink -f ~/accts`:${PYTHONPATH} && cd ~/accts/accts && python -tt -Wd ~/accts/accts.el6.py --dir ~/accts/results --email ${email} > ~/accts/results.out
-/bin/rm -f ~/.matplotlib/fontList.cache && /bin/rm -rf ${python_compiled} && /bin/rm -f ~/accts/results.out-${date} && /bin/rm -rf ~/accts/results-${date} && mkdir -p ~/accts/results-${date} && export PYTHONDONTWRITEBYTECODE=1 && export PYTHONPATH=`readlink -f ~/accts`:${PYTHONPATH} && python -tt -Wd ~/accts/accts.el6.py --dir ~/accts/results-${date} --email ${email} > ~/accts/results.out-${date}
+02 13 * * * date=`/bin/date +"\%Y-\%m-\%d"`&& /bin/rm -f ~/.matplotlib/fontList.cache && /bin/rm -rf ${python_compiled} && /bin/rm -f ~/accts/results.out-${date} && /bin/rm -rf ~/accts/results-${date} && mkdir -p ~/accts/results-${date} && export PYTHONDONTWRITEBYTECODE=1 && export PYTHONPATH=`readlink -f ~/accts`:${PYTHONPATH} && python -tt -Wd ~/accts/accts.el6.py --dir ~/accts/results-${date} --email ${email} > ~/accts/results.out-${date}
--- accts/mpi/gcc/submit.agts.py
+++ accts/mpi/gcc/submit.agts.py
@@ -3,5 +3,5 @@ import os
 def agts(queue):
     if 1:
         queue.add('examples.py',
-                  queueopts=['-l', 'nodes=2:ppn=8'],
-                  ncpus=1, walltime=1, deps=[])
+                  queueopts=['-l', 'nodes=2:ppn=16'],
+                  walltime=2, deps=[])
