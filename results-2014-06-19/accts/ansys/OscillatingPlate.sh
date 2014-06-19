50db990382993aa0505b67d1411fe64375702311
marcindulak <Marcin.Dulak@gmail.com>
2014-06-19 16:04:33 +0200

    fix ansys

--- results-2014-06-19/accts/ansys/OscillatingPlate.sh
+++ results-2014-06-19/accts/ansys/OscillatingPlate.sh
@@ -7,4 +7,4 @@ if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 export PATH=/appl/ansys/bin:${PATH}
 
 cp -rp /appl/ansys/14/v140/CFX/examples/OscillatingPlate.* .
-ansys -dis -cnf $PBS_NODEFILE -np `cat $PBS_NODEFILE | wc -l` -b -i OscillatingPlate.inp
+ansys -machines `echo $PBS_NODEFILE` -b -i OscillatingPlate.inp
--- results-2014-06-19/accts/ansys/mpitest.sh
+++ results-2014-06-19/accts/ansys/mpitest.sh
@@ -6,5 +6,5 @@ if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 # set path
 export PATH=/appl/ansys/bin:${PATH}
 
-ansys -dis -cnf $PBS_NODEFILE -np `cat $PBS_NODEFILE | wc -l` -mpitest
+ansys -machines `echo $PBS_NODEFILE` -mpitest
 
