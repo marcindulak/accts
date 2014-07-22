d63295d14f2f9df95dadcb456243eacf1764700a
marcindulak <Marcin.Dulak@gmail.com>
2014-07-22 15:08:38 +0200

    desmond works now

--- accts/schrodinger/desmond-3.4.0.2/dhfr.sh
+++ accts/schrodinger/desmond-3.4.0.2/dhfr.sh
@@ -4,13 +4,12 @@
 if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 
 # load the required modules
-module load mpi/gcc
+module load oldmpi/gcc
 export PATH=/appl/desmond/3.4.0.2/bin:$PATH
 export LD_LIBRARY_PATH=/appl/desmond/3.4.0.2/lib:$LD_LIBRARY_PATH
 export DESMOND_PLUGIN_PATH=/appl/desmond/3.4.0.2/lib/plugin:$LD_LIBRARY_PATH
 
 cp -rp /appl/desmond/3.4.0.2/share/samples/* .
 # http://wiki.hpc.ufl.edu/doc/Desmond_PBS
-mpirun -np 1 desmond --tpp `cat $PBS_NODEFILE | wc -l` --include dhfr.cfg --cfg boot.file=dhfr.dms --cfg mdsim.plugin.eneseq.name=dhfr.eneseq.threads
-#mpirun desmond --destrier mpi --tpp 1 --include dhfr.cfg --cfg boot.file=dhfr.dms --cfg mdsim.plugin.eneseq.name=dhfr.eneseq.mpi
+mpirun desmond --destrier mpi --tpp 1 --include dhfr.cfg --cfg boot.file=dhfr.dms --cfg mdsim.plugin.eneseq.name=dhfr.eneseq.mpi
 
--- accts/schrodinger/desmond-3.4.0.2/submit.agts.py
+++ accts/schrodinger/desmond-3.4.0.2/submit.agts.py
@@ -1,7 +1,7 @@
 import os
 
 def agts(queue):
-    if 0:
+    if 1:
         queue.add('dhfr.py',
                   queueopts=['-l', 'nodes=1:ppn=4'],
-                  ncpus=1, walltime=5, deps=[])
+                  ncpus=1, walltime=1, deps=[])
