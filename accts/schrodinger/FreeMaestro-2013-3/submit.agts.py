c9d2c86b1954b09a4d4d08271251535af292f5bf
marcindulak <Marcin.Dulak@gmail.com>
2014-07-22 12:33:05 +0200

    try anyway

--- accts/schrodinger/FreeMaestro-2013-3/submit.agts.py
+++ accts/schrodinger/FreeMaestro-2013-3/submit.agts.py
@@ -1,7 +1,7 @@
 import os
 
 def agts(queue):
-    if 0:
+    if 1:
         queue.add('para_testapp.py',
-                  queueopts=['-l', 'nodes=1:ppn=4'],
+                  queueopts=['-l', 'nodes=1:ppn=1'],
                   ncpus=1, walltime=1, deps=[])
