5fb1bd1a0567aa0bdbf07f599b8082463c29ef23
marcindulak <Marcin.Dulak@gmail.com>
2014-07-30 14:09:05 +0200

    run jaguar test

--- accts/schrodinger/2012Update2/H2O.sh
+++ accts/schrodinger/2012Update2/H2O.sh
@@ -6,7 +6,9 @@ if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 # load the required modules
 module load schrodinger/2012Update2
 
-cp -rp /opt/schrodinger/2012Update2/jaguar-v79515/samples/H2O.in .
+export SCHRODINGER_NODEFILE=$PBS_NODEFILE
+
+cp -p /opt/schrodinger/2012Update2/jaguar-v79515/samples/H2O.in .
 # run jaguar in parallel http://www.schrodinger.com/kb/1793
-jaguar run -NPROC `cat $PBS_NODEFILE | wc -l` -HOST `hostname` H2O.in
+jaguar run -DEBUG -WAIT -NPROC `cat $PBS_NODEFILE | wc -l` H2O.in
 
