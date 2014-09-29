227678b586faaa4afc8db8a047749ccdb2452475
marcindulak <Marcin.Dulak@gmail.com>
2014-09-29 15:12:21 +0200

    multithreaded openblas in R

--- accts/R/R-benchmark-25.sh
+++ accts/R/R-benchmark-25.sh
@@ -6,14 +6,15 @@ if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 # http://r.research.att.com/benchmarks/
 wget http://r.research.att.com/benchmarks/R-benchmark-25.R
 
-# Warning: currently R on HPC does not use multithreaded blas!
-# so two jobs below run equally fast
-
-# 1. multithreaded
+# 1. multithreaded (openblas)
 export OMP_NUM_THREADS=`cat $PBS_NODEFILE | wc -l`
-time /appl/R/bin/R CMD BATCH R-benchmark-25.R R-benchmark-25.R.${OMP_NUM_THREADS}
-# 2. single-threaded
+export GOTO_NUM_THREADS=`cat $PBS_NODEFILE | wc -l`
+export OPENBLAS_NUM_THREADS=`cat $PBS_NODEFILE | wc -l`
+time R CMD BATCH R-benchmark-25.R R-benchmark-25.R.${OMP_NUM_THREADS}
+# 2. single-threaded (openblas)
 export OMP_NUM_THREADS=1
-time /appl/R/bin/R CMD BATCH R-benchmark-25.R R-benchmark-25.R.${OMP_NUM_THREADS}
+export GOTO_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+time R CMD BATCH R-benchmark-25.R R-benchmark-25.R.${OMP_NUM_THREADS}
 # clean
 rm -f .RData
