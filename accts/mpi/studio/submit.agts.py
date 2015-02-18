7996d385eae5a7c15f9fcddab8ceb533359ba7d6
marcindulak <Marcin.Dulak@gmail.com>
2015-02-18 14:44:14 +0100

    run mpi on smaller number of cores

--- accts/mpi/studio/submit.agts.py
+++ accts/mpi/studio/submit.agts.py
@@ -3,5 +3,6 @@ import os
 def agts(queue):
     if 1:
         queue.add('examples.py',
-                  queueopts=['-l', 'nodes=2:ppn=16'],
+                  queueopts=['-l', 'nodes=2:ppn=4',
+                             '-W', 'x=nmatchpolicy:exactnode'],
                   walltime=2, deps=[])
