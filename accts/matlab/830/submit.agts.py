11c6649fb6227c985d439795829992335213a825
marcindulak <Marcin.Dulak@gmail.com>
2015-02-23 09:48:28 +0100

    run on 4 cores

--- accts/matlab/830/submit.agts.py
+++ accts/matlab/830/submit.agts.py
@@ -3,5 +3,5 @@ import os
 def agts(queue):
     if 1:
         queue.add('Integrate.py',
-                  queueopts=['-l', 'nodes=1:ppn=8'],
+                  queueopts=['-l', 'nodes=1:ppn=4'],
                   ncpus=1, walltime=2, deps=[])
