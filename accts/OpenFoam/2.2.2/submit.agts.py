e2051585c0f946864f58d0461a15b19a2f020070
marcindulak <Marcin.Dulak@gmail.com>
2015-02-26 15:13:29 +0100

    more walltime

--- accts/OpenFoam/2.2.2/submit.agts.py
+++ accts/OpenFoam/2.2.2/submit.agts.py
@@ -4,4 +4,4 @@ def agts(queue):
     if 1:
         queue.add('dam_break_long.py',
                   queueopts=['-l', 'nodes=4:ppn=4'],
-                  ncpus=1, walltime=3, deps=[])
+                  ncpus=1, walltime=4, deps=[])
